ec7256e5c5ad56c739d356b3841fb0450d331bf0
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-16 11:28:30 -0700

    Tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main -Entry point
+  *Return: always 0
+  */
+int main(void)
+{
+	int i;
+
+	char ch[] = "abcdefghijklmnopqrstuvwxyz\n";
+
+	for (i = 0 ; i < 27 ; i++)
+		putchar(ch[i]);
+	return (0);
+}
