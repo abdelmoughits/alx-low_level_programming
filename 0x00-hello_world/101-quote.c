0b036f756e7eb6e9d30dc9f4f490fc57aac02083
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-17 08:09:02 -0700

    Task7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
   *main -Entry point
   *Return: 1
   */
 int main(void)
 {
-	int i;
 	char mes[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int b = sizeof(mes);
 
-	for (i = 0 ; i < b ; i++)
-	{
-		putchar(mes[i]);
-	}
+	write(1, mes, b);
 	return (1);
 }
