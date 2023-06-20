5f0d0acfb98a39eeca2cafc67aaa12c334cf3f4e
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-20 14:04:12 -0700

    tadsk

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+/**
+  *_islower - Shows 1 if the input is an
+  *alphabetic character. Another casees shows
+  *0
+  *@c: the character in ASCII code
+  *Return: 1 if c alphabetic character. 0 for the rest
+  */
+int _isalpha(int c)
+{
+	if (!(isalpha(c) == 0))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
