7fb1a11f14ee36d5b3401ad26753545c338cef97
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-20 15:30:58 -0700

    Write a function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,19 +2,19 @@
 #include "main.h"
 /**
   *print_sign - Shows 1 and print 1 if the input is an
-  *positive number, return and print 0 if it is 0. Another casees return and shows
-  *-1
+  *positive number, return and print 0 if it is 0. Another
+  *casees return and shows -1
   *@n: the number in ASCII code
   *Return: 1 if positive number,0 if it is 0 . -1 if it is negative
   */
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if(c == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
