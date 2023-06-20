e2393bf774c8e72dd281217035d5531d1363e6f7
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-20 16:12:33 -0700

    test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+/**
+  *_abs -Shows the absolut value of a number
+  *@n: the input number as an intiger
+  *Return: the absolut value of the number
+  */
+int _abs(int n)
+{
+	return (abs(n));
+}
