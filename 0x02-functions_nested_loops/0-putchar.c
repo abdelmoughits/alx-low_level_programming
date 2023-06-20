76ae3e98a4fcf6eba147d6e34b1694ac1d95b80a
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-20 10:02:35 -0700

    hg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
   */
 int main(void)
 {
-	char put[] = "_putchar\n";
-	long unsigned int i;
-
-	for (i = 0 ; i < sizeof(put) ; i++)
-	{
-		_putchar(put[i]);
-	}
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("r");
 	return (0);
 }
