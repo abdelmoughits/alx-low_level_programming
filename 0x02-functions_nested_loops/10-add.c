ccd4d7428502f235ba425de1635771bf3a2f0aa8
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-20 19:23:17 -0700

    adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *add - Adds two integers and returns the result.
+  *@a: a number of type intiger
+  *@b: a number of type intiger
+  *Return: the sume of to intigers
+  */
+int add(int a, int b)
+{
+	return (a = b);
+}
