1c6f86d043461ac4cb99b5a8347b9ffd2e3e7c69
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-23 13:39:55 -0700

    task8 for 0x04projet

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *print_square - Prints a square, followed by a new line.
+  *@size: intiger variable
+  *Return: not exist
+  */
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 1 ; a <= size ; a++)
+	{
+		for (b = 1 ; b<= size ; b++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
