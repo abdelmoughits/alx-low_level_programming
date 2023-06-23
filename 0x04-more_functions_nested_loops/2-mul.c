e946482a12a7ed2cc4bec3a66cec4fb75d2a63f0
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-23 02:35:11 -0700

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+  *mul - Multiplies two integers
+  *@a: intiger variable
+  *@b: intiger variable
+  *Return: the multiplication of a and b
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
