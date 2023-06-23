489172c8b0ca500a7b23da96e148b3906e8ac9e1
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-23 14:09:16 -0700

    task 8_projet 0x04

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,6 +7,7 @@
 void print_square(int size)
 {
 	int a, b;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -15,7 +16,7 @@ void print_square(int size)
 	{
 		for (a = 1 ; a <= size ; a++)
 		{
-			for (b = 1 ; b<= size ; b++)
+			for (b = 1 ; b <= size ; b++)
 			{
 				_putchar(35);
 			}
