9716db873e02dca2a8aa58a97a1563fc42529683
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-26 09:12:36 -0700

    teast

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ *rest_to_ 98 - Takes a pointer to an int as parameter
+ *and updates the value it points to to 98
+ *@*n: pointer to an intiger variable
+ *Return : nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
