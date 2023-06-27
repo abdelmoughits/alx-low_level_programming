ce29d3824e314c6b35973a82ac4b7da3282bf0ab
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-27 12:24:51 -0700

    test

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *puts2 - Prints every other character of a string
+  *@str: pointer to a variable of type intiger
+  *Return: nothing
+  */
+void puts2(char *str)
+{
+	int num;
+
+	while (*str != '\0')
+	{
+		num = *str - '0';
+		printf("num = %d\n", num);
+		if (num % 2 == 0)
+		{
+			_putchar(num + 48);
+		}
+		str++;
+	}
+	_putchar('\n');
+}
