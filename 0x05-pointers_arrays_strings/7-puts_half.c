1e01c5efc9245edd97c5d367f58bc64e3cc676e8
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-27 18:23:10 -0700

    test

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *puts_half - Prints half of a string, followed by a new line
+  *@str: pointer to an intiger variable
+  *Return: nothing
+  */
+void puts_half(char *str)
+{
+	int i, count = 1;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	for (i = count / 2 ; i <= count ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
