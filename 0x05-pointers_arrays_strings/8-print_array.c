7ec72c6a9bc23994000a418a00b07fcab44ff1b2
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-28 11:40:28 -0700

    tset

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,17 +11,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n == 0)
+	if (n < 0)
 	{
-		printf("%d\n", a[0]);
+		break;
 	}
-	for (i = 0 ; i < n ; i++)
+	for (i = 1 ; i <= n ; i++)
 	{
-		if (i == n - 1)
+		if (i == n)
 		{
-			printf("%d\n", a[i]);
+			printf("%d\n", a[i - 1]);
 			continue;
 		}
-		printf("%d, ", a[i]);
+		printf("%d, ", a[i - 1]);
 	}
 }
