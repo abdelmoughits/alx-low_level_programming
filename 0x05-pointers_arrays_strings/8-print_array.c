e0de18afa4fe9457a849dce5fee4e9a9e9b29200
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-28 11:09:35 -0700

    tset

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+  *print_array - Prints n elements of an array of integers
+  *, followed by a new line.
+  *@a: pointer to an intiger array
+  *@n: a variable of type intiger
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n",a[i]);
+			continue;
+		}
+		printf("%d, ", a[i]);
+	}
+}
