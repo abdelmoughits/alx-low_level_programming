d6d5563e0028aa9f33c5e514583031b10f51a15c
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-06-30 02:59:58 -0700

    test

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+/**
+  *_strcat - Concatenates two strings
+  *@dest: pointer: to the char variable
+  *@src: pointer to the char variable
+  *Return:pointer to the resulting string dest
+  */
+char *_strcat(char *dest, char *src)
+{
+
+	int i = 0;
+	int b = 0;
+	bool tf = true;
+
+	while (1)
+	{
+		if (dest[i] == '\0' && tf == true)
+		{
+			tf = false;
+		}
+		if (tf == false)
+		{
+			dest[i] = src[b];
+			if (src[b] == '\0')
+			{
+				return (dest);
+			}
+			b++;
+		}
+		i++;
+	}
+}
