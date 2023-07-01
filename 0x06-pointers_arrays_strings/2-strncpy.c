503c4176d8db8fb311ded0863e1675b7df6442e3
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-01 02:57:17 -0700

    test

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
   *_strncpy - Copies a string.
   *@dest:pointer to a char variable
@@ -9,7 +10,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n && dest[i] != '\0' ; i++)
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 	{
 		dest[i] = src[i];
 	}
