be2b04a2578a84f637a73509943a147f99318655
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-02 12:31:02 -0700

    test

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,7 +23,7 @@ char *cap_string(char *str)
 		str[i - 1] == ')' ||
 		str[i - 1] == '{' ||
 		str[i - 1] == '}' ||
-		str[i - 1] == 0 ||
+		i == 0 ||
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
 		str[i - 1] == ' ')
