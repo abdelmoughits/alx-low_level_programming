7e3715a12e80d428660809e218e2dcc309ee41ea
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-02 09:55:05 -0700

    test

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,10 +8,10 @@ void reverse_array(int *a, int n)
 {
 	int i, b;
 
-	for (i = 0 ; i <= n / 2 && i >= 2 ; i++)
+	for (i = 0 ; i < n / 2 && n >= 2 ; i++)
 	{
 		b = a[i];
-		a[i] = a[n - i];
-		a[n - i] = b;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = b;
 	}
 }
