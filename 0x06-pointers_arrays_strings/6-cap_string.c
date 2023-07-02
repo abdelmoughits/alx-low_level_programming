329bc38e2328df55c1be420b63a88f8e58d6b7ef
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-02 12:08:28 -0700

    test

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+/**
+  *cap_string - Capitalizes all words of a string
+  *@str: pointer to a char variable
+  *Return: char value
+  */
+char *cap_string(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		while (!(str[i] <= 'z' && str[i] >= 'a'))
+		{
+			i++;
+		}
+		if (str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		str[i - 1] == 0 ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ' ')
+		{
+			str[i] -= 32;
+			i++;
+		}
+	}
+	return (str);
+}
