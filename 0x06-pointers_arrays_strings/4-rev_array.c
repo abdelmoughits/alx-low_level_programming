97d55bf9f2875fa3ad71df21b2d30155985b16b4
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-02 04:38:33 -0700

    test

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+  *reverse_array - Reverses the content of an array of integers.
+  *@a: pointer to an intiger variable
+  *@n: intiger variable
+  *Return: an intiger value
+  */
+void reverse_array(int *a, int n)
+{
+	int i;
+	char b;
+
+	for (i = 0 ; i <= (n - 1) / 2 ; i++)
+	{
+		b = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = b;
+	}
+}
