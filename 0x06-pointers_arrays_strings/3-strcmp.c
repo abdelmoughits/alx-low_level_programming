8370fac00114fccf9e101459b10854c64a529a43
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-02 03:53:16 -0700

    test

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,24 +7,10 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	i = 0;
-
-	while (1)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-		{
-			return (0);
-		}
-		else if (s1[i] == '\0')
-		{
-			return (-15);
-		}
-		else if (s2[i] == '\0')
-		{
-			return (15);
-		}
-		i++;
+		s1++;
+		s2++;
 	}
+	return (*s1 - *s2);
 }
