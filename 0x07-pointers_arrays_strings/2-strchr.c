1908d5076207842974335f9a3e762af07a406e11
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-03 13:15:25 -0700

    test

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+  *_strchr - Locates a character in a string
+  *@s: pointer to a char variable
+  *@c: char variable
+  *Return: char value
+  */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
