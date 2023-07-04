e8d329a8161614c30eac6a3fd38798b42534cc0e
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-04 10:51:43 -0700

    test

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,7 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, b, c, val = 0;
+	unsigned int i, b, c, val = 0;
 
 	for (i = 0 ; accept[i] != '\0' ; i++)
 	{	c = 0;
@@ -18,12 +18,4 @@ unsigned int _strspn(char *s, char *accept)
 			{	c = 1; }}
 		if (c == 0)
 		{	val++; }}
-	for (i = 0 ; s[i] != '\0' ; i++)
-	{       c = 0;
-		for (b = 0 ; accept[b] != '\0' ; b++)
-		{
-			if (accept[b] == s[i])
-			{       c = 1; }}
-		if (c == 0)
-		{       val++; }}
 	return (val); }
