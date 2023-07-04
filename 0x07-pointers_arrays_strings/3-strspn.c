09755009cd711473d41de61cd88adb72112066ab
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-04 08:24:58 -0700

    test

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,27 +8,17 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, b, val, c = 0;
-
-	val = 0;
+	int i, b, val = 0;
 
 	for (i = 0 ; accept[i] != '\0' ; i++)
 	{
-		c = 0;
 		for (b = 0 ; s[b] != '\0' ; b++)
 		{
 			if (accept[b] == s[i])
 			{
-				c = 1;
-				break;
+				val++;
 			}
 		}
-		printf("i = %d\n", i);
-		if (c == 0)
-		{
-			val++;
-			printf("val = %d\n", val);
-		}
 	}
-	return (val);
+	return ( i + b - val);
 }
