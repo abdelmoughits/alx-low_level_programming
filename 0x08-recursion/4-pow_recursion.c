809d126810b11f72262fc2353844138c1d91ade5
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-05 10:45:15 -0700

    test

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+/**
+  *_pow_recursion - Returns the value of x raised to the power of y.
+  *@x: intiger variable
+  *@y:intiger variable
+  *Returns the value of x raised to the power of y.
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(y - 1));
+}
