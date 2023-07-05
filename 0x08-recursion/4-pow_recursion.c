9b1dfc22280919cdb6a1c13dd9c905aa28f89ac7
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-05 10:52:55 -0700

    test

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
   *_pow_recursion - Returns the value of x raised to the power of y.
   *@x: intiger variable
   *@y:intiger variable
-  *Returns the value of x raised to the power of y.
+  *Return: the value of x raised to the power of y.
   */
 int _pow_recursion(int x, int y)
 {
