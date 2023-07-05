0480762deb38eb84b461df974c2c04fb27f1183d
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-05 09:49:20 -0700

    test

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+/**
+  *factorial - Returns the factorial of a given number.
+  *@n: intiger variable
+  *Return: the factorial of a given number
+  */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1);
+}
