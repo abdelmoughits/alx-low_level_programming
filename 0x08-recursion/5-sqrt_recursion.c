8c805e764c95abd064d8c604b3898c4c56a939e2
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-05 17:28:31 -0700

    test

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+int sqr(int n, int i);
+/**
+  *_sqrt_recursion - Returns the natural square root of a number.
+  *@n: intiger variable
+  *Return: the natural square root of a number.
+  */
+int _sqrt_recursion(int n)
+{
+	return(sqr(n, 1));
+}
+/**
+  *sqr - Returns the natural square root of a number.
+  *@n: intiger variable
+  *@i: intiger variable
+  *Return: the natural square root of a number.
+  */
+int sqr(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (sqr(n, i + 1);
+}
