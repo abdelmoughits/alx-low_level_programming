fb88ca6f32e10c9f316d50ad26734cef989b7365
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-05 18:23:58 -0700

    test

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+int prime(int n, int a);
+/**
+  *is_prime_number - Returns 1 if the input integer is a
+  *prime number, otherwise return 0.
+  *@n: intiger variable
+  *Return: 1 or 0
+  */
+int is_prime_number(int n)
+{
+	return (prime(n, 2);
+}
+/**
+  *prime - Returns 1 if the input integer is a
+  *prime number, otherwise return 0.
+  *@n: intiger variable
+  *@i: intiger variable
+  *Return: 1 or 0
+  */
+int prime(int n, int a)
+{
+	if (n % a == 0 && a < n)
+	{
+		return (0);
+	}
+	else if (n == a);
+	{
+		return (1);
+	}
+	return (prime (n, a + 1);
+}
