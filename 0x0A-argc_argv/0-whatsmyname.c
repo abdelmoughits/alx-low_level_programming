2aa9843432f7ed67ca3f6167cdaf2fc1bb9a592c
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-09 16:34:33 -0700

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+  *main - Prints its name, followed by a new line.
+  *@argc: intiger varible
+  *@argv: char array
+  *Return: always 0 (sucese)
+  */
+int main(int argc, char * argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
