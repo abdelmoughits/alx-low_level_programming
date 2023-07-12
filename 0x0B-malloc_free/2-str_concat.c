0e61bbf2f18028d4b9fdcd9a5127ee3fd63d7c96
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-12 15:38:09 -0700

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,19 +16,19 @@ char *str_concat(char *s1, char *s2)
 	{}
 	for (len2 = 0 ; s1 != NULL && s2[len2] != '\0'; len2++)
 	{}
-	len2++;
-	ptr = malloc(sizeof(char) * (len1 + len2));
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0 ; i <= len1; i++)
+	for (i = 0 ; i < len1; i++)
 	{
 		ptr[i] = s1[i];
 	}
-	for (i = 0 ; i <= len2 + len1; i++)
+	for (i = 0 ; i < len2 + len1; i++)
 	{
 		ptr[i + len1] = s2[i];
 	}
+	ptr[len1 + len2 + 1] = '\0';
 	return (ptr);
 }
