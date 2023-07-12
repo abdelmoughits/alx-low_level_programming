b248b6734504346acb8b9c1cb693638e27b5696b
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-11 17:17:39 -0700

    update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+  *_strdup - Contains a copy of the string given as a
+  *@str: pointer to a char variable
+  *Return: a pointer to the copy of str
+  */
+char *_strdup(char *str)
+{
+	int i, b;
+	char *array;
+
+	b = sizeof(str);
+	array = malloc(sizeof(char) * b);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < b ; i++)
+	{
+		array[i] = str[i];
+	}
+	return (array);
+}
