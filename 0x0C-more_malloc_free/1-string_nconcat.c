5f7597d676d111eeb9db9e57a7ba85a366917bf2
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-13 18:04:20 -0700

    updat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			len2++;
 		}
 	}
-	b = len1 + len2 + 1;
+	if (n >= len2)
+	{
+		b = len1 + len2 +;
+	}
+	else
+	{
+		b = len1 + n;
+	}
 	ptr = malloc(b);
 	if (ptr == NULL)
 	{
