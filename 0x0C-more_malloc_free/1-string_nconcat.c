74ad80adbace8da1fc6fbf4ccc34ddfdb143d435
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-13 18:21:02 -0700

    test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,42 +20,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		while (s1[len1])
 		{
-			len1++;
-		}
-	}
+			len1++; }}
 	if (s2 != NULL && n > 0)
 	{
 		i = 0;
 		while (s2[len2])
 		{
-			len2++;
-		}
-	}
+			len2++; }}
 	if (n >= len2)
 	{
-		b = len1 + len2 + 1;
-	}
+		b = len1 + len2 + 1; }
 	else
 	{
-		b = len1 + n;
-	}
+		b = len1 + n + 1; }
 	ptr = malloc(b);
 	if (ptr == NULL)
 	{
-		return (NULL);
-	}
+		return (NULL); }
 	for (i = 0 ; i < len1 ; i++)
 	{
-		ptr[i] = s1[i];
-	}
+		ptr[i] = s1[i]; }
 	for (i = 0 ; i < len2 ; i++)
 	{
 		ptr[i + len1] = s2[i];
 		if (i == n - 1)
 		{
-			break;
-		}
-	}
+			break; }}
 	ptr[b - 1] = '\0';
 	return (ptr);
 }
