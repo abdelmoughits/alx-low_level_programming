201966edc4b560018f3b43f712575c7b257980b3
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-18 10:34:12 -0700

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - a new type struct dog
+  *@name: char pointer
+  *@owner: char pointer
+  *@age: float variable
+  *Description: a new type struct dog with the following elements:
+  */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
