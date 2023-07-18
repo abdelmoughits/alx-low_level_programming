da3097b1ed0d1352cfa82362bad784e452566677
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-18 16:54:38 -0700

    teste

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+/**
+  *new_dog - Creates a new dog.
+  *@name: pointer to char variable
+  *@owner: pointer to char variable
+  *@age: float variable
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog == NULL)
+	{
+		return(NULL);
+	}
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
+
