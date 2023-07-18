99516d6e5dddf1f6bc41f1d2ef42e87bc065466e
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-18 16:07:33 -0700

    teste

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
+	/**
 	struct dog my_dog;
 
-	my_dog.name = "Poppy";
+	my_dog.name = NULL;
 	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	print_dog(&my_dog);
+	my_dog.owner = NULL;
+	print_dog(NULL);*/
 	return (0);
 }
