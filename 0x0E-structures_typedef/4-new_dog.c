8433bc6e8ae89ed170e3617beb69c59418508e06
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-18 17:56:39 -0700

    est

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,15 +5,18 @@
   *@name: pointer to char variable
   *@owner: pointer to char variable
   *@age: float variable
+  *Return: the adress of the variable of type dog_t
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+
 	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 	{
-		return(NULL);
+		free(new_dog);
+		return (NULL);
 	}
 	new_dog->name = name;
 	new_dog->age = age;
