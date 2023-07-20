f9d72eec4b3ad18a2ffa1ede0b6b56a6453ab4e2
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-19 21:13:53 -0700

    test

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
