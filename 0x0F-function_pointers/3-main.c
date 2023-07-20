98b8df886b1bb497ca15babf4ce900cd60b9eb05
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-07-19 21:05:20 -0700

    test

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *main - The main function
+  *@argc: intiger variable
+  *@argv: pointer to an array of char
+  *Return: always 0 (sucses)
+  */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+	if ((*op == '/' || *op == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (get_op_func(op) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+  *op_add - Calculat the sum
+  *@a: intiger variable
+  *@b: integer variable
+  *Return: the sum of a and b
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  *op_sub - Calculat the sub
+  *@a: intiger variable
+  *@b: integer variable
+  *Return: the sub of a and b
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  *op_mul - Calculat the mul
+  *@a: intiger variable
+  *@b: integer variable
+  *Return: the mul of a and b
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  *op_div - Calculat the div of and b
+  *@a: intiger variable
+  *@b: integer variable
+  *Return: the div of a and b
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  *op_mod - Calculat the mod of a and b
+  *@a: intiger variable
+  *@b: integer variable
+  *Return: the mod betwin a and b
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
