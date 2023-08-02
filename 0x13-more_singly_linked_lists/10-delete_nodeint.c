f60807ce566279e929c30df2a9bddc506f43f6cb
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-08-02 10:48:20 -0700

    update

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  *insert_nodeint_at_index - Inserts a new node at a given position.
+  *@head: pointer to a pointer to a listint_t variable
+  *@idx: unsigned intiger vareiable
+  *@n:intiger variable
+  *Return:  the address of the new node, or NULL if it failed
+  *or if it is not possible to add the new node at index idx
+  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *new;
+	listint_t *tpn;
+	unsigned int i = 0;
+
+	tpn = *head;
+	if (idx == 0)
+	{
+		new = tpn->next;
+		free(tpn);
+		tpn = new;
+		return (1);
+	}
+	while (i != idx - 1 && tpn)
+	{
+		tpn = tpn->next;
+		i++;
+	}
+	if (i != idx - 1 && !tpn)
+	{
+		return (-1);
+	}
+	new = tpn->next;
+	new = new->next;
+	free(tpn->next);
+	tpn->next = new;
+	return (1);
+}
