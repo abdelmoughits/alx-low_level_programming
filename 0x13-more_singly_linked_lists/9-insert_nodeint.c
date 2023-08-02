ec642ac07237560cd4f578e2f4606a8d0de41246
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-08-02 09:51:27 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	while (i != idx - 1 || !tpn)
+	while (i != idx - 1 && !tpn)
 	{
 		tpn = tpn->next;
 		i++;
