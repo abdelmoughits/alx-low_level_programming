65e13be06cf1181cb484399e4dd3e73251836689
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-08-03 13:09:46 -0700

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
   *binary_to_uint - Converts a binary number to an unsigned int.
   *@b: pointer to a char constent
-  *Return: the number or 0 there is one or more 
+  *Return: the number or 0 there is one or more
   *chars in the string b that is not 0 or 1 b is NULL
   */
 unsigned int binary_to_uint(const char *b)
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' &&  b[i] != '1')
 		{
-			return (333);
+			return (0);
 		}
 		num *= 2;
 		num += b[i] - 48;
