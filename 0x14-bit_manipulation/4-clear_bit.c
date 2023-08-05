ad0059eef2410dc63aa038564685af9fed0b3c8d
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-08-04 17:22:18 -0700

    update

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *get_bit - Sets the value of a bit to 1 at a given index.
+  *@index: variable
+  *@n: variable
+  *Return: 1 if it worked, or -1 if an error occurred
+  */
+int get_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 1;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	i = ~(i << index);
+	*n = i | *n;
+	return (1);
+}
