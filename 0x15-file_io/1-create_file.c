c3301c40e1e85df014dda8ddc941440ad0392ede
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-08-08 16:15:16 -0700

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+  *create_file - Creates a file.
+  *@filename: variable
+  *@text_content: variable
+  *Return: 1  or -1
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fp, n;
+
+	fp = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (fp == -1 || !filename)
+	{
+		return (-1);
+	}
+	for (n = 0; text_content[n]; n++)
+	write(fp, text_content, n);
+	close(fp);
+	return (1);
+}
