0e30e536d10af2a5a7cb4de35499de06f08b3b33
abdelmoughits <thepassengernumberfive7@gmail.com>
2023-08-08 16:27:47 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fp, n;
 
-	fp = open(filename, O_WRONLY | O_CREAT, 0600);
+	fp = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fp == -1 || !filename)
 	{
 		return (-1);
